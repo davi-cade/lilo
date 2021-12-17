707f5409273f26f1be204092c1a8aae6a2d5e3e7
thiago-b-rodrigues <thiagob.rodrigues2002@gmail.com>
2021-12-17 19:39:22 -0300

    Correção de erros

--- resources/views/Pre-Atividade/atividade.blade.php
+++ resources/views/Pre-Atividade/atividade.blade.php
@@ -1,98 +1,54 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Introdução I</title>
-
+    <title>Introdução</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-<link rel="stylesheet" href="\public\css\Atividade\pre-atividade\nav.css">
-
-<link rel="stylesheet" href="\public\css\Atividade\pre-atividade\painel.css">
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="\public\css\Atividade\pre-atividade\nav.css">
+    <link rel="stylesheet" href="\public\css\Atividade\pre-atividade\painel.css">
 </head>
 <body>
-    <div class="container">
-        <div class="row one">
-            <div class="cardup">
-                <h5 class="elemento"></h5>
-            </div>
-        </div>
-        <div class="box">
-
-            <div class="row tow">
-
-              <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-
-              <div class="box_one">
-                <div class="titulo">
-                    <h1 class="elemento">Sinal</h1>
-                  </div>
-                 
-                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                      <div class="carousel-inner">
-                        @foreach($cards as $key)
-                            <div class="carousel-item active">
-                                <video class="d-block w-100" src="{{asset($key->url_video)}}" controls autoplay muted loop></video>
-                                <h3>{{$key->title}}</h3>
-                            </div>
-                        @endforeach
+  <div class="container">
+    <div class="box">
+      <div class="row tow d-flex justify-content-center align-items-center">
+        <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+          <div class="box_one">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                @foreach($cards as $key)
+                  <div class="carousel-item  @if($loop->index == 0) active @endif">
+                    <video class="d-block w-100" src="{{asset($key->url_video)}}" controls autoplay muted loop></video>
+                    <div class="row one">
+                      <div class="cardup">
+                        <h3 class="elemento text-center">{{$key->title}}</h3>
                       </div>
-                      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                      </button>
-                      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                      </button>
-                  </div>
-              </div>
-              </div>
-              <button onclick="window.location.href='/module/{{$slug}}/{{$num}}/quizz'" class="btn1">PLAY</button>
-
-
-              <!-- container II -->
-              <!-- <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                <div class="box_two">
-                    <div class="titulo">
-                        <h1 class="elemento">Alternativa</h1>
                     </div>
                   </div>
-                </div> -->
-                <!-- final -->    
-                </div>
+                @endforeach
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden sr-only">Next</span>
+              </button>
+            </div>
           </div>
-        </div>
-  </div>
+          <button class="btn btn-primary btn-lg" onclick="window.location.href='/module/{{$slug}}/{{$num}}/quizz'" class="btn1">PLAY</button>  
+        </div>    
+      </div>
     </div>
-
+  </div>
     <onda>
-      <svg
-        class="waves"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20962 10601"
-      >
-        <path
-          class="wave-1"
-          fill="#01a09c"
-          d="M27489 8635c504 1667 831 3818-27 5680s-2901 3434-6193 3904-7834-161-12155-589c-4321-427-8421-649-10976-1537-2555-889-3565-2443-4071-4061-505-1619-505-3302 938-4835 1442-1533 4327-2916 6274-3122s2958 764 3877 1522 1746 1304 2738 1382 2148-312 2976-730 1327-864 2136-1014c810-151 1929-6 2757 206s1363 490 1978 775c614 284 1308 574 1947 535s1223-407 1935-875c712-469 1552-1037 2240-1266 688-228 1223-117 1832 530 608 646 1290 1828 1794 3495z"
-        />
-
-        <path
-          class="wave-2"
-          fill="#00cdc8"
-          d="M25681 10400c-171 822-305 1990-646 3115-341 1124-889 2206-1856 2808-968 602-2355 724-4668 1064s-5550 898-8520 987-5672-290-9037-859c-3366-568-7394-1326-9403-2118-2008-791-1996-1616-1892-3305 103-1689 298-4242 1412-5731 1113-1488 3146-1912 4953-1627 1808 284 3390 1276 4522 1878s1814 814 2423 552 1145-998 2333-1388c1187-389 3025-432 4266 275 1240 707 1884 2164 2861 2407 976 244 2285-726 3493-1308 1209-581 2316-775 3731-567 1415 207 3137 815 4290 1221 1153 407 1737 612 1926 953 189 342-18 820-188 1643z"
-        />
-
-        <path
-          class="wave-3"
-          fill="#01e8e2"
-          d="M30706 11822c-291 731-653 1801-1548 2972-895 1170-2321 2441-4988 3026-2667 586-6574 485-10469 541s-7778 268-10906-256-5501-1784-6906-3382c-1404-1598-1840-3533-1957-4910-118-1377 82-2195 1029-2854 946-659 2637-1158 4033-1002s2495 968 3440 1630 1737 1175 2905 1086c1169-89 2714-780 3810-1332 1095-552 1740-965 2665-820s2130 848 3110 1360c980 513 1734 837 2458 672s1416-819 2179-1050 1597-40 3058 269c1461 310 3550 739 5069 1177 1519 437 2469 883 2998 1112 529 228 639 240 584 440-55 201-274 591-564 1321z"
-        />
+      <svg class="waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20962 10601">
+        <path class="wave-1" fill="#01a09c" d="M27489 8635c504 1667 831 3818-27 5680s-2901 3434-6193 3904-7834-161-12155-589c-4321-427-8421-649-10976-1537-2555-889-3565-2443-4071-4061-505-1619-505-3302 938-4835 1442-1533 4327-2916 6274-3122s2958 764 3877 1522 1746 1304 2738 1382 2148-312 2976-730 1327-864 2136-1014c810-151 1929-6 2757 206s1363 490 1978 775c614 284 1308 574 1947 535s1223-407 1935-875c712-469 1552-1037 2240-1266 688-228 1223-117 1832 530 608 646 1290 1828 1794 3495z"/>
+        <path class="wave-2" fill="#00cdc8" d="M25681 10400c-171 822-305 1990-646 3115-341 1124-889 2206-1856 2808-968 602-2355 724-4668 1064s-5550 898-8520 987-5672-290-9037-859c-3366-568-7394-1326-9403-2118-2008-791-1996-1616-1892-3305 103-1689 298-4242 1412-5731 1113-1488 3146-1912 4953-1627 1808 284 3390 1276 4522 1878s1814 814 2423 552 1145-998 2333-1388c1187-389 3025-432 4266 275 1240 707 1884 2164 2861 2407 976 244 2285-726 3493-1308 1209-581 2316-775 3731-567 1415 207 3137 815 4290 1221 1153 407 1737 612 1926 953 189 342-18 820-188 1643z"/>
+        <path class="wave-3" fill="#01e8e2" d="M30706 11822c-291 731-653 1801-1548 2972-895 1170-2321 2441-4988 3026-2667 586-6574 485-10469 541s-7778 268-10906-256-5501-1784-6906-3382c-1404-1598-1840-3533-1957-4910-118-1377 82-2195 1029-2854 946-659 2637-1158 4033-1002s2495 968 3440 1630 1737 1175 2905 1086c1169-89 2714-780 3810-1332 1095-552 1740-965 2665-820s2130 848 3110 1360c980 513 1734 837 2458 672s1416-819 2179-1050 1597-40 3058 269c1461 310 3550 739 5069 1177 1519 437 2469 883 2998 1112 529 228 639 240 584 440-55 201-274 591-564 1321z"/>
       </svg>
     </onda>
 </body>
